80dfe0acb098fc6d4399fcc8562e8c1dca38fc0d
TecTrixer <tonihoevedes@gmail.com>
2024-08-07 15:21:34 +0200

    added c++ config

--- cpp/config.fish
+++ cpp/config.fish
@@ -1,51 +1,40 @@
-# Abbreviation for running just
-abbr j just
-
 # Folder where this repo is:
 set CP_REPO (realpath (dirname (status filename)))
 
-# Include cpp config
-source {$CP_REPO}/cpp/config.fish
-
-
 # This function adds another subtask to an already established contest
-function cp_add_task
+function cpp_add_task
     if test (count $argv) -lt 1
-        echo "You need to specify the task name, e.g. 'addcp b' will add 'b.rs'"
+        echo "You need to specify the task name, e.g. 'cp_cpp_add_task b' will add 'b.cpp'"
     else
         set i $argv[1]
-        echo "[[bin]]" >>Cargo.toml
-        echo "name=\"$i\"" >>Cargo.toml
-        echo "path=\"$i.rs\"" >>Cargo.toml
-        echo "" >>Cargo.toml
-        cp {$CP_REPO}/temp.rs ./$i.rs
+        cp {$CP_REPO}/temp.cpp ./$i.cpp
     end
 end
 
-# This function adds files for each subtask of the problem, e.g. from a.rs until h.rs
-function cp_add_contest_files
+# This function adds files for each subtask of the problem, e.g. from a.cpp until h.cpp
+function cpp_add_contest_files
     if test (count $argv) -lt 2
         echo "You need to specify the start and end of the files you want to add"
     else
         set start_val (printf "%d" "'$argv[1]'")
         set end_val (printf "%d" "'$argv[2]'")
         for val in (seq $start_val $end_val)
-            cp_add_task (printf "\\$(printf '%03o' $val)")
+            cpp_add_task (printf "\\$(printf '%03o' $val)")
         end
     end
 end
 
 # This function creates a new contest and optionally adds files for the subtasks
-function cp_new
+function cpp_new
     if test (count $argv) -ge 1
         mkdir $argv[1]
         cd $argv[1]
-        cp {$CP_REPO}/{Cargo.toml,.ignore,justfile,template.rs} .
+        cp {$CP_REPO}/{compile_flags.txt,.ignore,justfile} .
     else
         echo "Usage: [DIRNAME] [FROM] [TO], where [FROM] and [TO] are optional"
     end
     if test (count $argv) -eq 3
-        cp_add_contest_files $argv[2] $argv[3]
+        cpp_add_contest_files $argv[2] $argv[3]
         # also automatically start competitive companion server in the future
         comp-helper &
         bg
@@ -53,7 +42,7 @@ function cp_new
 end
 
 # this function evaluates a test and compares the output using delta
-function cp_eval_test
+function cpp_eval_test
     if test (count $argv) -eq 2
         echo "Testing task "(echo $argv[2] | string upper)
         echo ""
---
+++ cpp/temp.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+void solve(int T);
+int main() {
+  int T;
+  cin >> T;
+  while (T--)
+    solve(T);
+  return 0;
+}
+
+void solve(int T) {
+  
+}
